5af3829d3b6b1da8a84d3dbb942bb8f9a3660622
stanzyck_224 <denismuydinov2005@gmail.com>
2026-06-28 15:59:04 +0500

    fix: path added for env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,8 @@
-require("dotenv").config({ path: "../.env" });
+const path = require("path");
+
+require("dotenv").config({
+  path: path.join(__dirname, "../.env"),
+});
 
 module.exports = {
   BOT_TOKEN: process.env.BOT_TOKEN,
